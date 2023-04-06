8b46394b1f2ed0376e355fec5c057242be104a41
Linh Nguyen Bui Tai <linh.nguyenbuitai>
2023-04-06 18:05:32 +0700

    remove file react test

--- src/sum/sum.test.ts
+++ src/sum/sum.test.ts
@@ -4,5 +4,8 @@ import {sum} from './sum';
 describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+    expect(sum(2, 2)).toBe(4);
+    expect(sum(2, 2)).toBe(3);
+    expect(sum(1, 0)).toBe(0);
   });
 });
